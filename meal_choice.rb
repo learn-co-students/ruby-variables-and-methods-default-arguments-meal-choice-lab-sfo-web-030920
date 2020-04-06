c66c1678e9a31bf4263a27a238d7445f011c2392
github username <github email address>
2020-04-06 15:16:58 -0700

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,16 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+def meal_choice( food = "meat") 
+    return food
+end
+
+
+# describe '#meal_choice' do
+#   it 'should default to meat' do
+#     expect(meal_choice).to eq("meat")
+#   end
+
+#   it 'should allow you to set a meal' do
+#     expect(meal_choice("vegan")).to eq("vegan")
+#   end
+# end
